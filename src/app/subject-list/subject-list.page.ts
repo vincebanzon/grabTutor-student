2e12e72bbc0c1aaeb0f6d1b95bd454fe3ce390ce
Vince Banzon <vincebanzon@gmail.com>
2019-09-18 01:58:38 +0800

    Add price to each subjects

--- src/app/subject-list/subject-list.page.ts
+++ src/app/subject-list/subject-list.page.ts
@@ -38,47 +38,58 @@ export class SubjectListPage implements OnInit {
     return [{
         id: 0,
         name: 'Basic Math (+, -, x, %)',
-        sub: 'Learn Math in no time!'
+        sub: 'Learn Math in no time!',
+        price: '200'
       }, {
         id: 1,
         name: 'Addition',
-        sub: 'Learn Addition in no time!'
+        sub: 'Learn Addition in no time!',
+        price: '200'
       }, {
         id: 2,
         name: 'Subtraction',
-        sub: 'Learn how to subtract in no time!'
+        sub: 'Learn how to subtract in no time!',
+        price: '200'
       }, {
         id: 3,
         name: 'Basic Algebra',
-        sub: 'Learn the basics in Algebra.'
+        sub: 'Learn the basics in Algebra.',
+        price: '200'
       }, {
         id: 4,
         name: 'Advanced Algebra',
-        sub: "Learn the in's and out's of Algebra."
+        sub: "Learn the in's and out's of Algebra.",
+        price: '200'
       }, {
         id: 5,
         name: 'Basic Calculus',
-        sub: 'Learn the basic in calculus.'
+        sub: 'Learn the basic in calculus.',
+        price: '200'
       }, {
         id: 6,
         name: 'Advanced Calculus',
-        sub: 'Learn the calculus in different way.'
+        sub: 'Learn the calculus in different way.',
+        price: '200'
       }, {
         id: 7,
         name: 'Calculus for beginners',
-        sub: "If you don't know a thing in calculus yet, take this subject."
+        sub: "If you don't know a thing in calculus yet, take this subject.",
+        price: '200'
       }, {
         id: 8,
         name: 'Calculus for experts',
-        sub: 'Do you want to be an expert in calculus?'
+        sub: 'Do you want to be an expert in calculus?',
+        price: '200'
       }, {
         id: 9,
         name: 'Surface Area',
-        sub: 'Learn how to calculate Surface Area.'
+        sub: 'Learn how to calculate Surface Area.',
+        price: '200'
       }, {
         id: 10,
         name: 'Calculating Volume of an object',
-        sub: 'Learn how to calculate volume of an object.'
+        sub: 'Learn how to calculate volume of an object.',
+        price: '200'
     }]
   }
 
